4e827abfb43ee68be10d8edcb7bef06111b07d86
Guilherme COUTINHO <coutinho.gui.br@gmail.com>
2025-11-09 05:59:15 +0000

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,6 @@ export default createMiddleware({
 });
  
 export const config = {
-  // A list of all locales that are supported
-  locales: ['en', 'pt'],
- 
-  // Used when no locale matches
-  defaultLocale: 'en',
-  
   // Skip all paths that should not be internationalized. This example skips the
   // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
   matcher: ['/((?!api|_next|.*\\..*).*)']
