7c66a28b3997b806c63f7df07f097f4b1871e184
Guilherme COUTINHO <coutinho.gui.br@gmail.com>
2025-11-09 05:47:56 +0000

    Console Error Server
    Unable to find `next-intl` locale because the mi

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,18 @@
 import createMiddleware from 'next-intl/middleware';
-
+ 
 export default createMiddleware({
   locales: ['en', 'pt'],
   defaultLocale: 'en'
 });
-
+ 
 export const config = {
-  matcher: ['/', '/(pt|en)/:path*']
+  // A list of all locales that are supported
+  locales: ['en', 'pt'],
+ 
+  // Used when no locale matches
+  defaultLocale: 'en',
+  
+  // Skip all paths that should not be internationalized. This example skips the
+  // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
